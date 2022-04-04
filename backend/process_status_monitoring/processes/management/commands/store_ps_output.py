67f03e19bffecfbe34afc323b8b7d6d034fab74b
r0ti <ajnesvirajkurcu@gmail.com>
2022-04-04 20:09:20 +0200

    (be): Added Django management command whih calls bash script, parses output and saves data into db

---
+++ backend/process_status_monitoring/processes/management/commands/store_ps_output.py
@@ -0,0 +1,71 @@
+from typing import List, Dict
+import subprocess
+
+from django.core.management.base import BaseCommand
+
+from process_status_monitoring.processes.models import Process
+
+
+class Command(BaseCommand):
+    help = """Fetches the output from the `output_ps` bash script,
+              parses the str data
+              and saves into db."""
+
+    def handle(self, *args, **options):
+        ps = subprocess.run(["./scripts/output_ps.sh"], check=True, capture_output=True)
+        output = subprocess.run(
+            ["sed", "-e", "s/\\s\\+/-|-/g", "-e", "s/$/\\n/"],
+            input=ps.stdout,
+            capture_output=True,
+        ).stdout.decode("utf-8")
+        self.save_to_db(self.parse_output(output))
+
+    def parse_output(self, output: str) -> List[Dict[str, str]]:
+        """
+        Parses the output of `output_ps` bash script into a list[dict], each dict
+        representing the parsed process information from each row of the output.
+
+        Keys are mapped to Process model field names, parsed from the first line of
+        the process output.
+        """
+
+        output = list(filter(None, output.replace("\n", "-|-").split("-|-")))
+        headers = [h.replace("#", "").replace("%", "").lower() for h in output[0:12]]
+        headers_len = len(headers)
+        ps_rows = []
+        command_str = ""
+        counter = 0
+
+        # FIXME: Bugprone/hackish method to parse and concat parts of command
+        # after spliting the str
+        for v in output[headers_len:]:
+            if counter >= headers_len - 1:
+                try:
+                    int(v)
+                except ValueError:
+                    command_str += f" {v}"
+                else:
+                    ps_rows.append(command_str.strip())
+                    ps_rows.append(v)
+                    counter = 0
+                    command_str = ""
+            else:
+                ps_rows.append(v)
+            if v == output[-1]:
+                ps_rows.append(command_str.strip())
+            counter += 1
+        raw_data = [*[ps_rows[i : i + 12] for i in range(0, len(ps_rows), 12)]]
+        parsed_output = []
+        for r in raw_data:
+            d = {}
+            for i, v in enumerate(r):
+                d[headers[i]] = v
+            parsed_output.append(d)
+        return parsed_output
+
+    def save_to_db(self, parsed_output: List[Dict[str, str]]) -> None:
+        """
+        Stores parsed output into db.
+        """
+        # Bulk saving (1 query), due to performance and speed
+        Process.objects.bulk_create([Process(**r) for r in parsed_output])
